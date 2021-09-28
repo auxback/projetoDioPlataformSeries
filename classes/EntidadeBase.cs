821524f068f9502d3addae735cb3804c90ff5d8b
Antony Mário <antonymr018@gmail.com>
2021-09-28 16:14:31 -0300

    Projeto Plataforma de Series (DIO) - Console App

---
+++ classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+// por algum motivo ele tira "classes de DIO.Series"
+namespace DIO.Series
+{
+    public class EntidadeBase
+    {
+        public int Id {get; protected set;}
+    }
+}
---
+++ classes/SerieRepositorio.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using DIO.series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        
+    }
+}
